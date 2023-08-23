a395d50599aff72523a39152cafe24cb71add8c4
xiaoliangshun <xlscomputer@163.com>
2023-08-23 21:50:50 +0800

    修改oauth2_demo项目， 修改了UserConroller,解析了获取到的jwt内容

--- oauth2_demo/src/main/java/org/example/controller/UserController.java
+++ oauth2_demo/src/main/java/org/example/controller/UserController.java
@@ -1,8 +1,13 @@
 package org.example.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 //资源服务器下的资源
 @RestController
@@ -11,12 +16,21 @@ public class UserController {
 
     /**
      * 获取当前用户
+     * 对jwt继续接续
      * @param authentication
      * @return
      */
     @RequestMapping("/getCurrentUser")
-    public Object getCurrentUser(Authentication authentication){
-        return authentication.getPrincipal();
+    public Object getCurrentUser(Authentication authentication, HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        String token = header.substring(header.indexOf("bearer") + 7);
+
+        Claims body = Jwts.parser()
+                .setSigningKey("test_key".getBytes(StandardCharsets.UTF_8))
+                .parseClaimsJws(token)
+                .getBody();
+        return body;
+//        return authentication.getPrincipal();
     }
 
 
